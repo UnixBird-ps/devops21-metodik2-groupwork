815eab95407b4a8dcb918b856f31b4feb2a8b784
UnixBird-ps <88337865+UnixBird-ps@users.noreply.github.com>
2022-06-03 13:16:36 +0200

    Rewrote create script

--- create-docker-tools.sh
+++ create-docker-tools.sh
@@ -2,15 +2,22 @@
 
 ## Copy docker-tools-template to docker-tools
 ## but keep old ssh-key sub folder...
-cp -r docker-tools/copy-to-docker-container/ssh-key ssh-key 2> /dev/null
-rm -r docker-tools 2> /dev/null
-cp -r docker-tools-template docker-tools 2> /dev/null
-cp -r ssh-key docker-tools/copy-to-docker-container/ 2> /dev/null
-rm -r ssh-key > /dev/null 2> /dev/null
-echo "./docker-tools/start.sh" > start
-echo "./docker-tools/stop.sh" > stop
-chmod 777 start
-chmod 777 stop
+
+chmod 777 ./docker-tools-template/start.sh
+chmod 777 ./docker-tools-template/stop.sh
+
+ln -s ./docker-tools-template/start.sh start
+ln -s ./docker-tools-template/stop.sh stop
+
+rm -r docker-tools
+cp -r docker-tools-template docker-tools
+cp -r ssh-key docker-tools/copy-to-docker-container/
+
+#echo "./docker-tools/start.sh" > start
+#echo "./docker-tools/stop.sh" > stop
+
+#chmod 777 start
+#chmod 777 stop
 
 echo ""
 echo "Docker tools created! :)"
---
+++ delete-docker-tools.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+## Copy docker-tools-template to docker-tools
+## but keep old ssh-key sub folder...
+
+rm -r docker-tools
+rm start
+rm stop
+
+echo ""
+echo "Docker tools removed! ;|"
+echo ""
