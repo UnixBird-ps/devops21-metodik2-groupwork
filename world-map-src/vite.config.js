4ac715bceb27fd01884104702d627e0c108230f6
UnixBird-ps <88337865+UnixBird-ps@users.noreply.github.com>
2022-06-05 19:22:24 +0200

    Adapting to 'consulting firm's' idea

--- world-map-src/vite.config.js
+++ world-map-src/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(
+   {
+      plugins: [ react() ],
+      server:
+      {
+         port: process.env.PORT,
+         hmr: false
+      }
+   }
+);
