e732333bf91daf145db3b9a4d2be6a6650cc6dfc
UnixBird-ps <88337865+UnixBird-ps@users.noreply.github.com>
2022-06-06 18:40:39 +0200

    Added all dev branches

--- remove-docker-tools.sh
+++ remove-docker-tools.sh
@@ -3,8 +3,8 @@
 rm start
 rm stop
 
-[ -f docker-tools/copy-to-docker-container/ssh-key/id_ed25519.pub ] \
-&& [ -f docker-tools/copy-to-docker-container/ssh-key/id_ed25519 ] \
+[[ ! -f ssh-key/id_ed25519.pub || ! -f ssh-key/id_ed25519 ]] \
+&& [[ -f docker-tools/copy-to-docker-container/ssh-key/id_ed25519.pub && -f docker-tools/copy-to-docker-container/ssh-key/id_ed25519 ]] \
 && cp --preserve=mode,timestamps -rf docker-tools/copy-to-docker-container/ssh-key ssh-key 2> /dev/null
 
 rm -rf docker-tools 2> /dev/null
