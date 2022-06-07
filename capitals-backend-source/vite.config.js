a995e567dedce88ef4879f39fc378af30590c676
UnixBird-ps <88337865+UnixBird-ps@users.noreply.github.com>
2022-06-07 11:55:32 +0200

    Created a branch for 'capitals' backend with devs's code

---
+++ capitals-backend-source/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: process.env.PORT,
+    hmr: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+})
