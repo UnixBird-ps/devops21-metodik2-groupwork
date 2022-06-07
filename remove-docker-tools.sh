5c7255007b1ee8f22fe92a6e07d88317d62a0fee
UnixBird-ps <88337865+UnixBird-ps@users.noreply.github.com>
2022-06-07 19:57:45 +0200

    Trying to fix problem with missing ssh key

--- remove-docker-tools.sh
+++ remove-docker-tools.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm start
-rm stop
+rm start 2> /dev/null
+rm stop 2> /dev/null
 
 [[ ! -f ssh-key/id_ed25519.pub || ! -f ssh-key/id_ed25519 ]] \
-&& cp --preserve=mode,timestamps -rf docker-tools/copy-to-docker-container/ssh-key ssh-key 2> /dev/null
+&& cp -r docker-tools/copy-to-docker-container/ssh-key ssh-key 2> /dev/null
 
-rm -rf docker-tools 2> /dev/null
+rm -r docker-tools 2> /dev/null
 
 echo ""
 echo "Docker tools removed! ;|"
