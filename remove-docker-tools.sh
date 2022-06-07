d1a8fe53c4fb3b7571cd36dcf038b5f60f46cc2a
UnixBird-ps <88337865+UnixBird-ps@users.noreply.github.com>
2022-06-07 21:59:44 +0200

    Changed port for capitals

--- remove-docker-tools.sh
+++ remove-docker-tools.sh
@@ -4,9 +4,9 @@ rm start 2> /dev/null
 rm stop 2> /dev/null
 
 [[ ! -f ssh-key/id_ed25519.pub || ! -f ssh-key/id_ed25519 ]] \
-&& cp -r docker-tools/copy-to-docker-container/ssh-key ssh-key 2> /dev/null
+&& cp -r docker-tools/copy-to-docker-container/ssh-key ssh-key #2> /dev/null
 
-rm -r docker-tools 2> /dev/null
+rm -r docker-tools #2> /dev/null
 
 echo ""
 echo "Docker tools removed! ;|"
