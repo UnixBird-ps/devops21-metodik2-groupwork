bb2fbcc03c035aa94eb18696ebf84df5118bd6f2
UnixBird-ps <88337865+UnixBird-ps@users.noreply.github.com>
2022-06-07 20:49:52 +0200

    Update from common branch

--- remove-docker-tools.sh
+++ remove-docker-tools.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-rm start
-rm stop
+rm start 2> /dev/null
+rm stop 2> /dev/null
 
 [[ ! -f ssh-key/id_ed25519.pub || ! -f ssh-key/id_ed25519 ]] \
-&& [[ -f docker-tools/copy-to-docker-container/ssh-key/id_ed25519.pub && -f docker-tools/copy-to-docker-container/ssh-key/id_ed25519 ]] \
-&& cp --preserve=mode,timestamps -rf docker-tools/copy-to-docker-container/ssh-key ssh-key 2> /dev/null
+&& cp -r docker-tools/copy-to-docker-container/ssh-key ssh-key 2> /dev/null
 
-rm -rf docker-tools 2> /dev/null
+rm -r docker-tools 2> /dev/null
 
 echo ""
 echo "Docker tools removed! ;|"
