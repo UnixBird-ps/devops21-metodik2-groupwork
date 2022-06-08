27d34516db220f34a43e7aa45870927ab2079794
UnixBird-ps <88337865+UnixBird-ps@users.noreply.github.com>
2022-06-08 17:26:16 +0200

    Updated to common settings

--- create-docker-tools.sh
+++ create-docker-tools.sh
@@ -3,8 +3,10 @@
 chmod 700 ./docker-tools-template/start.sh
 chmod 700 ./docker-tools-template/stop.sh
 
-cp -r docker-tools/copy-to-docker-container/ssh-key ssh-key 2> /dev/null
-rm -r docker-tools 2> /dev/null
+[[ ! -f ssh-key/id_ed25519.pub || ! -f ssh-key/id_ed25519 ]] \
+&& cp -r docker-tools/copy-to-docker-container/ssh-key ssh-key 2> /dev/null
+
+rm -rf docker-tools 2> /dev/null
 cp -r docker-tools-template docker-tools 2> /dev/null
 cp -r ssh-key docker-tools/copy-to-docker-container/ 2> /dev/null
 
