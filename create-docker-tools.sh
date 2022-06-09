4403ad384748bd1ed4cbc55d3dbf700c38d061fc
UnixBird-ps <88337865+UnixBird-ps@users.noreply.github.com>
2022-06-09 13:18:08 +0200

    Adapted teacher's idea

--- create-docker-tools.sh
+++ create-docker-tools.sh
@@ -1,18 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-chmod 700 ./docker-tools-template/start.sh
-chmod 700 ./docker-tools-template/stop.sh
-
-cp -r docker-tools/copy-to-docker-container/ssh-key ssh-key 2> /dev/null
-rm -r docker-tools 2> /dev/null
-cp -r docker-tools-template docker-tools 2> /dev/null
-cp -r ssh-key docker-tools/copy-to-docker-container/ 2> /dev/null
-
+## Copy docker-tools-template to docker-tools
+## but keep old ssh-key sub folder...
+cp -r docker-tools/copy-to-docker-container/ssh-key ssh-key
+rm -r docker-tools
+cp -r docker-tools-template docker-tools
+cp -r ssh-key docker-tools/copy-to-docker-container/
+rm -r ssh-key
 echo "./docker-tools/start.sh" > start
 echo "./docker-tools/stop.sh" > stop
-
-chmod 700 start
-chmod 700 stop
+chmod 770 start
+chmod 770 stop
 
 echo ""
 echo "Docker tools created! :)"
