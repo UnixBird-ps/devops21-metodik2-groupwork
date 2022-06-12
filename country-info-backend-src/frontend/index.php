4a7116599ab9dbb71a9731d2abc8160f2069d94d
UnixBird-ps <88337865+UnixBird-ps@users.noreply.github.com>
2022-06-12 22:03:14 +0200

    Reverting back to devs's old code because the new one does not connect to db despite many hours of try and error

--- country-info-backend-src/frontend/index.php
+++ country-info-backend-src/frontend/index.php
@@ -1,41 +1,44 @@
-<!DOCTYPE html>
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "mysql";
+    $database = "countries";
+?><!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=<, initial-scale=1.0">
   <title>Country Info</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<?php
-  // Comment in for error reporting
-  /*ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);*/
+  <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
-  // Include code for database connection
-  include_once('./db-connect.php');
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
 
-  // Get country from query parameter
-  $country = isset($_GET["country"]) ? $_GET["country"] : '';
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
 
-  // Check for info about the country in the database
-  $stmt = $db->prepare('SELECT * FROM info WHERE name=?');
-  $stmt->execute([$country]);
-  $result = $stmt->fetchAll();
-  
-  // Echo info if found
-  if($country != '' && count($result) === 0){
-    echo 'Can not find info about '.$country.'...';
-  }
-  else if($country != ''){
-    echo $result[0]['infotext'];
-  }
-  else {
-    echo 'Click on a country to get info...';
-  }
+    // Not really using db after this
+
+    $country = isset($_GET["country"]) ? $_GET["country"] : '';
+
+    if(!file_exists("./info/".$country.".html")){
+      echo "<h1>Click a country...</h1>";
+    }
+    else {
+      $contents = file_get_contents("./info/".$country.".html");
+      $pos = strpos($contents,'coordinates');
+      echo "<h1>$country</h1";
+      echo $contents;
+    }
 ?>
-<script src="main.js"></script>
 </body>
 </html>
